498f2e475be8f5217995e264dae92a834dc43b5d
Sabbir Ahmed <69301433+sabbirahmed404@users.noreply.github.com>
2024-12-01 20:39:14 +0600

    added icon, change routing, problem page.tsx no default

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
-
+//import Image from "next/image";
+import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <main>
-      <button variant="destructive"> Hello </button>
+      <h1 className="text-4xl">GUB-EMS</h1>
+      <Button variant="destructive"> Hello </Button>
     </main>
   );
 }
